3fdea93d2c233b714892dcd157488123b4938d33
saransh1220 <prod.blazeinstinct@gmail.com>
2025-11-29 19:45:38 +0530

    ux imrpovement

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { provideZoneChangeDetection } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { routes } from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideZoneChangeDetection(), provideRouter(routes)],
+  providers: [
+    provideZoneChangeDetection(),
+    provideRouter(
+      routes,
+      withInMemoryScrolling({
+        scrollPositionRestoration: 'top',
+      })
+    ),
+  ],
 }).catch((err) => console.error(err));
