6c9cb6c9e8d2b164aa7fa897d0f04f63352eff2b
saransh1220 <prod.blazeinstinct@gmail.com>
2025-11-30 13:43:57 +0530

    fix cart system and styling

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,6 +1,7 @@
-import { Component, output } from '@angular/core';
+import { Component, inject, output } from '@angular/core';
 
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { CartService } from '../../services/cart.service';
 import { ThemeToggleComponent } from '../theme-toggle/theme-toggle';
 
 @Component({
@@ -13,6 +14,9 @@ import { ThemeToggleComponent } from '../theme-toggle/theme-toggle';
 export class HeaderComponent {
   cartClick = output<void>();
   isMobileMenuOpen = false;
+  cartService = inject(CartService);
+
+  constructor() {}
 
   toggleMobileMenu() {
     this.isMobileMenuOpen = !this.isMobileMenuOpen;
