40b7fe095f5874a60b78c8b6e5dcc03f00f45982
saransh1220 <prod.blazeinstinct@gmail.com>
2026-05-14 01:05:17 +0530

    fix: update production API URL and Razorpay key in environment configuration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,6 @@
 export const environment = {
   production: true,
-  // i will update when have to deploy
-  apiUrl: 'http://localhost:8080',
-  razorpayKeyId: 'prodId',
+  apiUrl: 'https://blueprint-audio-backend.onrender.com',
+  razorpayKeyId: 'rzp_test_SBnb5oqTfLoxEP',
   googleClientId: '825462170598-au5lsfaq2fq7215khagugl1d5ovk97ct.apps.googleusercontent.com',
 };
