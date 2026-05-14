58fb63f12a409a5b55b492fe706cb9e3ba50683d
saransh1220 <prod.blazeinstinct@gmail.com>
2026-05-14 13:48:25 +0530

    fix: update API URL in production environment configuration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://blueprint-audio-backend.onrender.com',
+  apiUrl: 'https://api-qa.waveyard.studio',
   razorpayKeyId: 'rzp_test_SBnb5oqTfLoxEP',
   googleClientId: '825462170598-au5lsfaq2fq7215khagugl1d5ovk97ct.apps.googleusercontent.com',
 };
